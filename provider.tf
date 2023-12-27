f73954eac24b0c3cea669106aafa6eac90242b09
root <root@Lucas.localdomain>
2023-12-27 10:44:09 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.31.0"
     }
+  }
+  backend "s3" {
+    bucket = "comunidadedevops-eks"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
